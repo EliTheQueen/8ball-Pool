c9d1ff4da866e9bb3249e01a9dbc3bac84227bea
elnaznosrati <elnaznosrati39@gmail.com>
2026-05-30 15:19:21 +0330

    add player class

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Player {
+    private String name;
+    private Ball.Group group;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Ball.Group getGroup() { return group; }
+    public void setGroup(Ball.Group group) { this.group = group; }
+    public int getScore() { return score; }
+    public void addScore() { score++; }
+    public void reset() { group = null; score = 0; }
+    public String groupText() { return group == null ? "Open" : group.toString(); }
 }
